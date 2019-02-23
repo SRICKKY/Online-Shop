90328b76aff371f0990b3ad0a108a90f5a9d85f1
Srikant <kumar_srikant@outlook.com>
2019-02-23 22:59:01 +0530

    Code formatting

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,7 +1,8 @@
 import os
+
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','myshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 app = Celery('myshop')
 
